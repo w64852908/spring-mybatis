b59ddfd6757719a3758520dbc296a0f03c69614f
lanxiang <lanxiang@lanxiangdeMacBook-Pro.local>
2017-05-11 20:21:38 +0800

    save code

---
+++ com.lanxiang.thrift/src/main/java/com/lanxiang/thrift/demo/JavaClient.java
@@ -0,0 +1,15 @@
+package com.lanxiang.thrift.demo;
+
+/**
+ * Created by lanxiang on 2017/5/11.
+ */
+public class JavaClient {
+
+    private static void perform() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
